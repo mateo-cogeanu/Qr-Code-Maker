73e139295b19a2d8aacf62448c147d1f211dfa8a
mateo-cogeanu <github.mateo@cogeanu.com>
2024-12-26 09:12:16 +0100

    Initial Commit

---
+++ Qr Code Maker/Qr_Code_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qr_Code_MakerApp.swift
+//  Qr Code Maker
+//
+//  Created by Mateo Cogeanu on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Qr_Code_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Qr Code MakerTests/Qr_Code_MakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Qr_Code_MakerTests.swift
+//  Qr Code MakerTests
+//
+//  Created by Mateo Cogeanu on 26.12.2024.
+//
+
+import Testing
+@testable import Qr_Code_Maker
+
+struct Qr_Code_MakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
